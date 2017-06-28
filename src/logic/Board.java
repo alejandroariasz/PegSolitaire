6cfde9297912ffaf7110a7fe4606bea0a8080709
huaariasza <huaariasza@unal.edu.co>
2017-06-28 12:11:07 -0500

    First commit

---
+++ src/logic/Board.java
@@ -0,0 +1,55 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Board {
+	
+	private List<Peg> pegs;
+	
+	public Board()
+	{
+		initBoard();
+	}
+	
+	public void initBoard()
+	{
+		this.pegs = new ArrayList<Peg>()
+		{
+			
+		};
+	}
+	
+	public List<Tile> getAvailableMovements()
+	{
+		
+		return null;
+	}
+	
+	public HashMap<Tile, Peg> getLeftMovement(Peg peg)
+	{
+		if(peg.getX() <= 1)
+			return null;
+		if(peg.getX() < 5 && (peg.getY() < 3 || peg.getY() > 5))
+			return null;
+		
+		if(getPegToRemove(new Tile(peg.getX()-1, peg.getY())) !=  null)
+		{
+			HashMap<Tile, Peg> movement = new HashMap<Tile,Peg>();
+			movement.put(new Tile(2,3), getPegToRemove(new Tile(peg.getX()-1, peg.getY())));
+		}
+		return null;
+		
+	}
+	
+	public Peg getPegToRemove(Tile tile)
+	{
+		int pegIndex = pegs.indexOf(new Peg(tile.getX(), tile.getY()));
+		if(pegIndex >= 0)
+			return pegs.get(pegIndex);
+		
+		return null;
+	}
+
+}
