d18ce1e8cc43fff7b02109da1ce63a46691d572a
fabian <fabian@fabian-VM>
2017-06-28 18:45:08 -0500

    new processing window

---
+++ src/view/Game.java
@@ -0,0 +1,28 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet{
+	
+	@Override
+    public void settings(){
+        size(600,600);
+    }
+
+    @Override
+    public void setup(){ 
+    	
+    }
+    
+    @Override
+    public void draw(){
+    
+    }
+
+	public static void main(String[] args) {
+		
+		PApplet.main("view.Game");
+
+	}
+
+}
