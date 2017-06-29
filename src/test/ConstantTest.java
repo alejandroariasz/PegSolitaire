60daecd32454976404c7c917336acab44518f037
Fabian Monsalve <fabtics@gmail.com>
2017-06-29 17:49:43 -0500

    new unit test methods

--- src/test/ConstantTest.java
+++ src/test/ConstantTest.java
@@ -5,48 +5,35 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import logic.Board;
+import logic.Point;
+import logic.Tile;
+import util.Constant;
+
 public class ConstantTest {
-
-	/**
-	 * Test method for {@link util.Constant#getLatinCross()}.
-	 */
-	@Test
-	public void testGetLatinCross() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link util.Constant#getCross()}.
-	 */
-	@Test
-	public void testGetCross() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link util.Constant#getButaco()}.
-	 */
-	@Test
-	public void testGetButaco() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link util.Constant#getPyramid()}.
-	 */
-	@Test
-	public void testGetPyramid() {
-		fail("Not yet implemented");
-	}
 
 	/**
 	 * Test method for {@link util.Constant#setBoardPegs(java.util.List)}.
 	 */
 	@Test
 	public void testSetBoardPegs() {
-		fail("Not yet implemented");
+		List<Point> points = new ArrayList<Point>();
+		Board boardTest = new Board();
+		
+		points.add(new Point(2,1));
+		points.add(new Point(3,3));
+		points.add(new Point(3,5));			
+		boardTest.setBoard(Constant.setBoardPegs(points));
+		assertTrue("Tile set", boardTest.getBoard().indexOf(new Tile(new Point(2,1), true)) != -1);
+		assertTrue("Tile set", boardTest.getBoard().indexOf(new Tile(new Point(3,3), true)) != -1);
+		assertTrue("Tile set", boardTest.getBoard().indexOf(new Tile(new Point(3,5), true)) != -1);
+		assertTrue("Tile unset", boardTest.getBoard().indexOf(new Tile(new Point(3,4), true)) == -1);		
+		
 	}
 
 }
--- src/test/TileTest.java
+++ src/test/TileTest.java
@@ -7,54 +7,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import logic.Point;
+import logic.Tile;
+
 public class TileTest {
 
-	/**
-	 * Test method for {@link logic.Tile#Tile(logic.Point, boolean)}.
-	 */
-	@Test
-	public void testTile() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link logic.Tile#getPoint()}.
-	 */
-	@Test
-	public void testGetPoint() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link logic.Tile#setPoint(logic.Point)}.
-	 */
-	@Test
-	public void testSetPoint() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link logic.Tile#hasPeg()}.
-	 */
-	@Test
-	public void testHasPeg() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link logic.Tile#changePeg(boolean)}.
-	 */
-	@Test
-	public void testChangePeg() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link logic.Tile#equals(java.lang.Object)}.
-	 */
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		Tile tileTest = new Tile(new Point(3,1), false);
+		assertTrue("equal tile", tileTest.equals(new Tile(new Point(3,1), false)));
+		assertFalse("different tile", tileTest.equals(new Tile(new Point(3,1), true)));
+		assertFalse("different tile", tileTest.equals(new Tile(new Point(3,2), false)));
 	}
 
 }
