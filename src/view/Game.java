f56330fe0ec8b03841db0410bf3bba4f5a5d1b8c
fabian <fabian@fabian-VM>
2017-06-28 20:21:26 -0500

    files and background board

--- src/view/Game.java
+++ src/view/Game.java
@@ -1,22 +1,34 @@
 package view;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Game extends PApplet{
 	
+	PImage boardIMG;
+	
 	@Override
     public void settings(){
-        size(600,600);
+		// load board images
+    	boardIMG = loadImage("board.jpg");
+        size(boardIMG.width,boardIMG.height);
     }
 
     @Override
     public void setup(){ 
+    	imageMode(CENTER);
     	
+    	drawBoard();
     }
     
     @Override
     public void draw(){
+     //System.out.println(mouseX + " " + mouseY);
+    }
     
+    public void drawBoard()
+    {
+      image(boardIMG, width/2, height/2);
     }
 
 	public static void main(String[] args) {
